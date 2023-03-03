52491de4dfe65356f20685b885517ecb2ea5bb32
andres06-hub <andressierrarojas7@gmail.com>
2023-03-03 11:55:48 -0500

    CREATED: module for signup

---
+++ src/app/modules/signup/signup.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignupComponent } from './signup.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { SignupRoutingModule } from './signup-routing.module';
+
+@NgModule({
+  declarations: [
+    SignupComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    SignupRoutingModule
+  ]
+})
+export class SignupModule { }
