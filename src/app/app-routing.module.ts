43b937fde589ee159e2be27106c0bff6b8067dc1
andres06-hub <andressierrarojas7@gmail.com>
2023-03-03 12:38:55 -0500

    ADD: Route not-found and HttpCLient

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'signup', loadChildren: ()=> import('./modules/signup/signup.module').then(m => m.SignupModule) },
+  { path: 'not-found', loadChildren: () => import('./modules/not-found/not-found.module').then(m => m.NotFoundModule) },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
 ];
 
