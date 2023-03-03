97589cd3303f8d9f30e7e6e5d7c85be9af94e84e
andres06-hub <andressierrarojas7@gmail.com>
2023-03-03 16:54:53 -0500

    MODIFIED-ADD: router and added formsmodule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'signup', loadChildren: ()=> import('./modules/signup/signup.module').then(m => m.SignupModule) },
-  { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) },
-  { path: 'myPosts', loadChildren: () => import('./modules/my-msg/my-msg.module').then(m => m.MyMsgModule) },
+  { path: 'signup', loadChildren: ()=> import('./modules/signup/signup.module').then(m => m.SignupModule)},
+  { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)},
+  { path: 'myPosts', loadChildren: () => import('./modules/my-msg/my-msg.module').then(m => m.MyMsgModule)},
+  { path: 'allPosts', loadChildren: () => import('./modules/posts/posts.module').then(m => m.PostsModule) },
   { path: 'not-found', loadChildren: () => import('./modules/not-found/not-found.module').then(m => m.NotFoundModule) },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { RouterModule } from '@angular/router';
     AppRoutingModule,
     RouterModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
